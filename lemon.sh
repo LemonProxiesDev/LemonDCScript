e81bf81e050d54009f352be8d9c478f83abc6e4f
LemonProxiesDev <70445676+LemonProxiesDev@users.noreply.github.com>
2020-08-29 19:05:33 +0200

    Add files via upload
    0.0.1 v

---
+++ lemon.sh
@@ -0,0 +1,34 @@
+# update package list and upgrade new versions of packages existing on the machine.
+sudo apt update && sudo apt upgrade -y
+
+# install Squid Proxy Server.
+sudo apt install squid -y
+
+# start and enable squid service.
+sudo systemctl start squid
+sudo systemctl enable squid
+
+# install apache utils.
+sudo apt install apache2-utils -y
+
+# create a passwd file in a same dir. to squid.conf.
+sudo touch /etc/squid/passwd
+
+# change the ownership to proxy user.
+sudo chown proxy /etc/squid/passwd
+
+# create user for proxy service.
+# sudo htpasswd /etc/squid/passwd-PROXY_USER
+
+/usr/bin/htpasswd -b -c /etc/squid/passwd lemonproxies premium
+
+echo "LemonProxies user created !"
+
+# replace old squid.conf with new conf. file.
+sudo cp squid.conf /etc/squid
+
+# restart squid service.
+sudo systemctl restart squid
+
+# done.
+ echo "LemonProxies Script DONE !"
