9ea65d35b03215252e899af58ea9bc742e950cc3
LemonProxiesDev <70445676+LemonProxiesDev@users.noreply.github.com>
2020-08-31 03:28:53 +0200

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -1,3 +1,18 @@
+
+# Get Datacenter IP ()
+ip=$(wget -O - -q https://icanhazip.com/)
+
+# Get Datacenter Port ( Default )
+port=3128
+
+# Get Datacenter username ( Default )
 user=lemonproxies
-passw=$(pwgen -A 6 1)
+
+# Genarate Random psw
+passw=$(pwgen -A 4 1)
+
 sudo htpasswd -b -c /etc/squid/passwd ${user} ${passw}
+echo -e "\e[1;93mUSER CREATED! \e[0m
+
+echo -e "\e[1;93mDONE ! \e[0m"
+echo -e "\e[1;93mYour Proxy is : ${ip}:${port}:${user}:${passw}\e[0m"
