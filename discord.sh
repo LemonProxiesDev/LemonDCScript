4ec7a3ec60f1674903f15f0cdc2fb36983baff02
LemonProxiesDev <70445676+LemonProxiesDev@users.noreply.github.com>
2020-09-01 12:56:59 +0200

    Update discord.sh

--- discord.sh
+++ discord.sh
@@ -2,7 +2,7 @@
 message="$@"
 ## format to parse to curl
 ## echo Sending message: $message
-msg_content=\"$message\"
+msg_content=\"$message TESTS\"
 
 ## discord webhook
 url='https://discordapp.com/api/webhooks/750307253541797960/8GkGqqlZhfUfJH-PthUq7LraaCYoMYknP9iFMYBRiM0iyATYonATNsDqfGVN7qJ1HvKf'
