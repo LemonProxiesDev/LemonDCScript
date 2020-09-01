b0fc2f84f32dd885d65dd8770dab211894ef69d2
LemonProxiesDev <70445676+LemonProxiesDev@users.noreply.github.com>
2020-09-01 13:19:01 +0200

    Update discord.sh

--- discord.sh
+++ discord.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
-message="$@"
-## format to parse to curl
-## echo Sending message: $message
-msg_content=\"$message TESTS\"
+# -X POST - use POST method.
+# -H "Content-Type: application/json" - header tells server you're sending JSON data.
+# -d '{"username": "test", "content": "hello"}' - sets request data.
+curl -X POST -H "Content-Type: application/json" -d '{"username": "test", "content": "hello"}' "https://discordapp.com/api/webhooks/750307253541797960/8GkGqqlZhfUfJH-PthUq7LraaCYoMYknP9iFMYBRiM0iyATYonATNsDqfGVN7qJ1HvKf"
 
-## discord webhook
-url='https://discordapp.com/api/webhooks/750307253541797960/8GkGqqlZhfUfJH-PthUq7LraaCYoMYknP9iFMYBRiM0iyATYonATNsDqfGVN7qJ1HvKf'
-curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_content}" $url
+# To make command more readable you can split it to multiple lines using backslash `\`
+# and set webhook url as variable, so you don't need to paste it over and over again.
+# Also you can add the variable to your `.*rc` file, so it persists on console reloads.
+export WEBHOOK_URL="https://discordapp.com/api/webhooks/750307253541797960/8GkGqqlZhfUfJH-PthUq7LraaCYoMYknP9iFMYBRiM0iyATYonATNsDqfGVN7qJ1HvKf"
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"username": "test", "content": "hello"}' \
+  $WEBHOOK_URL
